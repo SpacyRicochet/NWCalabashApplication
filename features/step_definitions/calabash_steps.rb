2899fef428402dbc1eb25d04297536dbc699d9e2
Bruno Scheele <bruno@noodlewerk.com>
2012-04-16 12:49:32 +0200

    Added step definitions for keyboard visibility.

--- features/step_definitions/calabash_steps.rb
+++ features/step_definitions/calabash_steps.rb
@@ -1,1 +1,16 @@
 require 'calabash-cucumber/calabash_steps'
+
+Then /^I should see the keyboard$/ do
+  res = element_exists( "keyboardAutomatic" )
+  if not res
+    screenshot_and_raise "Expected keyboard to be visible."
+  end
+end
+
+Then /^I should not see the keyboard$/ do
+  res = element_exists( "keyboardAutomatic" )
+  if res
+    screenshot_and_raise "Expected keyboard to not be visible."
+  end
+end
+
